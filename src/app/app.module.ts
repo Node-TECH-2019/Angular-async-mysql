0906922d005d2cb6141b77ac187f849dcf7de152
Mori-Chan <59697686+Mori-Chan@users.noreply.github.com>
2021-03-02 23:41:59 +0900

    23:41

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { CommentDatePipe } from './pipes/comment-date.pipe';
 
 @NgModule({
@@ -14,6 +15,8 @@ import { CommentDatePipe } from './pipes/comment-date.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     FormsModule
   ],
   providers: [],
